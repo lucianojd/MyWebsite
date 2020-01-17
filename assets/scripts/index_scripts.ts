a29cb40ba8564913c0fb61f3a8741b3a574135a9
Luciano De Gianni <lucdegianni@gmail.com>
2020-01-16 22:40:43 -0800

    Wrote scripts for loading menu and main pages.

---
+++ assets/scripts/index_scripts.ts
@@ -0,0 +1,32 @@
+function change_container_contents(id: string, page_num: number): void {
+    let page_list = [
+        "assets/html_pages/home_page.html",
+        "assets/html_pages/education_page.html",
+        "assets/html_pages/skills_page.html",
+        "assets/html_pages/projects_page.html",
+        "assets/html_pages/menu.html"
+    ];
+
+    $(id).load(page_list[page_num]);
+
+    // Don't save the page if it's the menu.
+    if(page_num == 4) { return; }
+
+    localStorage.setItem("currentPage", page_num.toString());
+}
+
+function determine_page_to_load(id: string): void {
+    let page_num: number = +localStorage.getItem("currentPage");
+
+    // Set the current page to the home page if no page is set.
+    if(!page_num) {
+        change_container_contents(id, 0);
+        return;
+    }
+    
+    change_container_contents(id, page_num);
+}
+
+function load_menu(): void {
+    change_container_contents("#menu-container", 4);
+}
