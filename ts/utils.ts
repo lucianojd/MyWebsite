26799e851dd797b210653a1819485ed007096e81
Luciano De Gianni <lucdegianni@gmail.com>
2020-03-21 21:12:06 -0700

    Initial commit. Added start of function.

---
+++ ts/utils.ts
@@ -0,0 +1,12 @@
+export function loadEducationTable(content: string): void {
+    var sesStrVar = "currEducationTable"
+
+    // Used for organizing classes as they're read.
+    class Class {
+        name: string
+        code: string
+        URL: string
+    }
+
+    $.get()
+}
