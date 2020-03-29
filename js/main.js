27f44187e11cfe939455f40dc5d77408fd78d1ff
Luciano De Gianni <lucdegianni@gmail.com>
2020-03-29 13:10:07 -0700

    Compiled.

--- js/main.js
+++ js/main.js
@@ -1,62 +1,120 @@
+import { createEducationTable } from "./utils.js";
+// Global variables
+var trgts = {
+    menu: "#menu-container",
+    page: "#page-container"
+};
+var sesStrVar = {
+    currPg: "currentPage",
+    currEdTab: "currentEducationTable",
+    currExList: "currentExperienceList"
+};
+var path = {
+    html: "/html/"
+};
 // Set up page.
 $(document).ready(function () {
-    var trgts = {
-        menu: "#menu-container",
-        page: "#page-container"
-    };
-    var sesStrVar = {
-        currPg: "currentPage"
-    };
-    var htmlPath = "html/";
-    // Load menu and add button functionality.
-    $(trgts.menu).load("html/menu.html");
-    // Setup menu buttons.
-    $(trgts.menu).on("click", "button", function () {
-        var URL = htmlPath + this.id + ".html";
-        // Setup the button to load the page.
-        $(trgts.page).load(URL);
-        sessionStorage.setItem(sesStrVar.currPg, this.id);
-        // Attach a page loading function to the button.
-        pageLoadFunctionSelector(this.id);
-    });
+    // Load menu.
+    $(trgts.menu).load(path.html + "menu.html");
     //  Load the last page open.
     var defaultPage = "home";
     if (!sessionStorage.getItem(sesStrVar.currPg)) {
         sessionStorage.setItem(sesStrVar.currPg, defaultPage);
     }
     var currentPage = sessionStorage.getItem(sesStrVar.currPg);
-    $(trgts.page).load(htmlPath + currentPage + ".html");
+    $(trgts.page).load(path.html + currentPage + ".html");
 });
-// Used to grab a function to execute when loading a page.
-function pageLoadFunctionSelector(pgName) {
-    switch (pgName) {
+// Handle button presses.
+export function buttonHandler(button) {
+    switch (button.id) {
         case "education":
-            loadEducation();
-            break;
+        case "experience":
         case "home":
-            loadHome();
-            break;
         case "interests":
-            loadInterests();
-            break;
         case "projects":
-            loadProjects();
-            break;
-        case "skills":
-            loadSkills();
+            // Load and store the page as the current page.
+            var page = path.html + button.id + ".html";
+            $(trgts.page).load(page);
+            sessionStorage.setItem(sesStrVar.currPg, button.id);
             break;
         default:
-            // Do nothing.
+            alert("That button is not currently working. Please try another.");
             break;
     }
 }
+// Handle select changes.
+export function handleSelectChange(select, ttlID, itemID) {
+    // Get name and value of selected item.
+    var currValue = select.value;
+    var currName = $("#" + select.id + " :selected").text();
+    // Determine which select item it is.
+    switch (select.id) {
+        case "education_select":
+            // Get the select specific information.
+            var sesStorage = sesStrVar.currEdTab;
+            var ttlString = "Courses completed at ";
+            // Load the education table.
+            createEducationTable(currValue, function (htmlString) {
+                $(itemID).html(htmlString);
+            });
+            break;
+        default:
+            alert("Item could not be loaded. Make another selection");
+            break;
+    }
+    // Set title.
+    $(ttlID).text(ttlString + currName);
+    //Set session storage.
+    sessionStorage.setItem(sesStorage, currValue);
+}
+export function handleSelectOnReady(selectID, ttlID, itemID) {
+    switch (selectID) {
+        case "#education_select":
+            var sesStorage = sesStrVar.currEdTab;
+            var ttlString = "Courses completed at ";
+            break;
+        case "#experience_select":
+            var sesStorage = sesStrVar.currExList;
+            var ttlString = "";
+            break;
+        default:
+            alert("Could not load previous item.");
+            return;
+    }
+    // Check for last loaded item.
+    var currItem = sessionStorage.getItem(sesStorage);
+    // Set default item if none is present.
+    if (!currItem) {
+        currItem = $(selectID).val();
+        sessionStorage.setItem(sesStorage, currItem);
+    }
+    // Switch select to current item.
+    $(selectID).val(currItem);
+    // Set title.
+    $(ttlID).text(ttlString + $(selectID + " :selected").text());
+    // Create the item.
+    switch (selectID) {
+        case "#education_select":
+            createEducationTable(currItem, function (htmlString) {
+                $(itemID).html(htmlString);
+            });
+    }
+}
+// Functions attach functions to controls specific to each page.
 function loadEducation() {
-}
-function loadHome() {
-}
-function loadInterests() {
-}
-function loadProjects() {
-}
-function loadSkills() {
+    // Change table contents when the drop down changes.
+    $(document).on("change", "select: #education_select", function () {
+        // Grabs vurrent selections value.
+        var currSelection = $(this).val();
+        // Grabs text of the current selection.
+        var currName = $("#" + this.id + " :selected").text();
+        // Fill the table.
+        createEducationTable(currSelection, function (htmlString) {
+            $("#course_table").html(htmlString);
+        });
+        // Set title.
+        $("#education_title").text("Courses completed at " + currName);
+        // Set the session storage for the education table.
+        sessionStorage.setItem("currentEducationTable", currName);
+    });
 }
--- js/utils.js
+++ js/utils.js
@@ -1,8 +1,56 @@
-export function loadEducationTable(id, content) {
-    var sesStrVar = "currEducationTable";
-    var Class = /** @class */ (function () {
-        function Class() {
+export function createEducationTable(id, callback) {
+    var docPath = "/resources/data/" + id + "_courses.txt";
+    // Read file.
+    $.ajax({
+        url: docPath,
+        dataType: "text"
+    }).done(function (data) {
+        data = data.split("\r\n");
+        // Create html.
+        var Class = /** @class */ (function () {
+            function Class(code, name, url) {
+                this.code = code;
+                this.name = name;
+                this.url = url;
+            }
+            return Class;
+        }());
+        // Creat array of class objects.
+        var htmlStr = "";
+        var classes = [];
+        for (var i = 0, j = 0; i < data.length; i++) {
+            // i++ necessary to skip "\n".
+            classes[j++] = new Class(data[i++], data[i++], data[i++]);
         }
-        return Class;
-    }());
+        // Sort classes.
+        classes.sort(function (a, b) {
+            if (a.code > b.code) {
+                return 1;
+            }
+            if (a.code < b.code) {
+                return -1;
+            }
+            return 0;
+        });
+        // Build html string.
+        var c;
+        for (var _i = 0, classes_1 = classes; _i < classes_1.length; _i++) {
+            c = classes_1[_i];
+            htmlStr += "<tr><td><a href=\"" + c.url + "\" target==\"_blank\">";
+            htmlStr += c.code + "</a></td>";
+            htmlStr += "<td>" + c.name + "</td></tr>";
+        }
+        callback(htmlStr);
+    });
+}
+export function createExperienceList(id, callback) {
+    var docPath = "/resources/data/" + id.toLowerCase() + ".txt";
+    // Open document.
+    $.ajax({
+        url: docPath,
+        dataType: "text"
+    }).done(function (data) {
+        data = data.split("\r\n");
+        console.log(data);
+    });
 }
